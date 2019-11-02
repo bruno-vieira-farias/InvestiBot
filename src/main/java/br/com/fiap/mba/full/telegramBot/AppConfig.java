c894c1bd49783a64f5c193247a333e05a6458bed
Bruno Farias <contato.brunofarias@gmail.com>
2019-11-02 18:24:35 -0300

    WIP...

---
+++ src/main/java/br/com/fiap/mba/full/telegramBot/AppConfig.java
@@ -0,0 +1,21 @@
+//package br.com.fiap.mba.full.telegramBot;
+//
+//import com.pengrad.telegrambot.TelegramBot;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@EnableAutoConfiguration
+//@EnableConfigurationProperties
+//@Configuration
+//public class AppConfig {
+//
+//    @Bean
+//    public TelegramBot telegramBot(){
+//        return new TelegramBot("897746755:AAG3Kh_LpbGeeJLiorrn6cVx5I_d6UQefJU");
+//    }
+//
+//}
+//
+//
--- src/test/java/br/com/fiap/mba/full/telegramBot/MainApplicationTests.java
+++ src/test/java/br/com/fiap/mba/full/telegramBot/MainApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.fiap.mba.full.demo;
+package br.com.fiap.mba.full.telegramBot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MainApplicationTests {
 
 	@Test
 	void contextLoads() {
